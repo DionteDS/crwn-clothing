1d4196f64ec588492df56d065e0626795aa6aa44
DionteDS <Dionte_sb@yahoo.com>
2021-03-18 15:33:36 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,19 +12,39 @@ const config = {
   measurementId: "G-FGSX7FQEH2",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  try {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData,
+        });
+      } catch (error) {
+        console.log("error creating user", error.message);
+      }
+    }
+    return userRef;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
-firestore
-  .collection("users")
-  .doc("EJMYpGKaFH14iAjv4tem")
-  .collection("carItems")
-  .doc("rknvSdJInItWeRbGkHX6");
-
-firestore.doc("/users/EJMYpGKaFH14iAjv4tem/carItems/rknvSdJInItWeRbGkHX6");
-firestore.collection("/users/EJMYpGKaFH14iAjv4tem/carItems/");
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
