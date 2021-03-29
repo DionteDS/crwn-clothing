7df22ae3a1bfef3b27effd63e56c255bdc2955bc
DionteDS <Dionte_sb@yahoo.com>
2021-03-28 18:06:52 -0700

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.components.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51Ia8tzF02U8Iyn1icRUeKC6wsfL5obGragjSFt3heVpZNaVgbris4vtjXv3UuozlHF8kt0nfVRCl3LZ5LOKEHDLz00odM6UAaT";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
